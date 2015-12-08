c0eb213fac468b66b1590c18a24516aad56836fe
Marcin <marcin.zajda@gmail.com>
2015-12-08 22:57:04 +0100

    Connecting modules

--- app/src/main/java/app.java
+++ app/src/main/java/app.java
@@ -1,11 +1,20 @@
 /**
  * Created by Marcin on 2015-12-08.
  */
+
+
+import othersmodel.Employee;
+import othersmodel.Salary;
+
+import java.math.BigDecimal;
+
 public class app {
 
     public static void main(String[] args) {
 
+        Employee emp = new Employee(1, "Marcin", "Zajda", new Salary(new BigDecimal(5000)), "Junior Developer");
 
+        System.out.println(emp.getOccupation());
 
     }
 }
